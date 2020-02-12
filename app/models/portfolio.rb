24a489ff825db6824e763d6d030ae8999391d2af
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-02-12 13:23:41 -0500

    Implemented concern for data features

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.react
@@ -6,4 +7,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails, -> { where(subtitle: "Ruby On Rails") }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
+    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "150")
+  end
 end
